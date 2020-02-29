860f12c29aaf7f29866732cf23d62d87db24d6dd
Steven Etgen <setgen5@gmail.com>
2020-02-28 20:22:26 -0500

    Added assets and base code from tutorial

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,16 +1,173 @@
 import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
-  private exampleObject: ExampleObject;
+  background: Phaser.GameObjects.TileSprite;
+  ship1: Phaser.GameObjects.Sprite;
+  ship2: Phaser.GameObjects.Sprite;
+  ship3: Phaser.GameObjects.Sprite;
+  config: Phaser.Types.Core.GameConfig;
+  powerUps: any;
 
-  constructor() {
-    super({ key: 'MainScene' });
+    constructor() {
+      super({ key: 'MainScene' });
+    }
+  
+    create() {
+  
+      this.background = this.add.tileSprite(0, 0, <number>this.config.width, <number>this.config.height, "../../assets/images/background.png");
+      this.background.setOrigin(0, 0);
+  
+      this.ship1 = this.add.sprite(<number>this.config.width / 2 - 50, <number>this.config.height / 2, "../../assets/images/ship.png");
+      this.ship2 = this.add.sprite(<number>this.config.width / 2, <number>this.config.height / 2, "../../assets/images/ship2.png");
+      this.ship3 = this.add.sprite(<number>this.config.width / 2 + 50, <number>this.config.height / 2, "../../assets/images/ship3.png");
+  
+  
+      this.anims.create({
+        key: "ship1_anim",
+        frames: this.anims.generateFrameNumbers("../../assets/images/ship.png", {
+          start: 2,
+          end: 3
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+      this.anims.create({
+        key: "ship2_anim",
+        frames: this.anims.generateFrameNumbers("../../assets/images/ship2.png", {
+          start: 2,
+          end: 3
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+      this.anims.create({
+        key: "ship3_anim",
+        frames: this.anims.generateFrameNumbers("../../assets/images/ship3.png", {
+          start: 2,
+          end: 3
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+  
+      this.anims.create({
+        key: "explode",
+        frames: this.anims.generateFrameNumbers("../../assets/spritesheets/explosion.png", {
+          start: 2,
+          end: 3
+        }),
+        frameRate: 20,
+        repeat: 0,
+        hideOnComplete: true
+      });
+  
+      this.ship1.play("ship1_anim");
+      this.ship2.play("ship2_anim");
+      this.ship3.play("ship3_anim");
+  
+      this.ship1.setInteractive();
+      this.ship2.setInteractive();
+      this.ship3.setInteractive();
+  
+      this.input.on('gameobjectdown', this.destroyShip, this);
+  
+      this.add.text(20, 20, "Playing game", {
+        font: "25px Arial",
+        fill: "yellow"
+      });
+  
+      // POWER UPS
+  
+      //2.1 Two Animations for the power ups
+      this.anims.create({
+        key: "red",
+        frames: this.anims.generateFrameNumbers("../../assets/spritesheets/power-up.png", {
+          start: 0,
+          end: 1
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+      this.anims.create({
+        key: "gray",
+        frames: this.anims.generateFrameNumbers("../../assets/spritesheets/power-up.png", {
+          start: 2,
+          end: 3
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+  
+      // 3.1
+      this.physics.world.setBoundsCollision();
+  
+      this.powerUps = this.physics.add.group();
+  
+      // 2.2 Add multiple objects
+      var maxObjects = 4;
+      for (var i = 0; i <= maxObjects; i++) {
+        var powerUp = this.physics.add.sprite(16, 16, "../../assets/spritesheets/power-up.png");
+        this.powerUps.add(powerUp);
+         powerUp.setRandomPosition(0, 0, <number>this.game.config.width, <number>this.game.config.height);
+  
+        // set random animation
+        if (Math.random() > 0.5) {
+          powerUp.play("red");
+        } else {
+          powerUp.play("gray");
+        }
+  
+        // setVelocity
+        powerUp.setVelocity(100, 100);
+        // 3.2
+        powerUp.setCollideWorldBounds(true);
+        // 3.3
+       powerUp.setBounce(1);
+  
+      }
+  
+    }
+  
+    update() {
+  
+      this.moveShip(this.ship1, 1);
+      this.moveShip(this.ship2, 2);
+      this.moveShip(this.ship3, 3);
+  
+      this.background.tilePositionY -= 0.5;
+  
+    }
+  
+    moveShip(ship, speed) {
+      ship.y += speed;
+      if (ship.y > <number>this.config.height) {
+        this.resetShipPos(ship);
+      }
+    }
+  
+    resetShipPos(ship) {
+      ship.y = 0;
+      var randomX = Phaser.Math.Between(0, <number>this.config.width);
+      ship.x = randomX;
+    }
+  
+    destroyShip(pointer, gameObject) {
+      gameObject.setTexture("explosion");
+      gameObject.play("explode");
+    }
+  
+  
   }
+//   private exampleObject: ExampleObject;
 
-  create() {
-    this.exampleObject = new ExampleObject(this, 0, 0);
-  }
+//   constructor() {
+//     super({ key: 'MainScene' });
+//   }
 
-  update() {
-  }
-}
+//   create() {
+//     this.exampleObject = new ExampleObject(this, 0, 0);
+//   }
+
+//   update() {
+//   }
+// }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -1,4 +1,41 @@
 export default class PreloadScene extends Phaser.Scene {
+//   constructor() {
+//     super({ key: 'PreloadScene' });
+//   }
+
+//   preload(){
+//     this.load.image("background", "../../assets/images/background.png");
+//     //
+//     this.load.spritesheet("ship", "../../assets/spritesheets/ship.png",{
+//       frameWidth: 16,
+//       frameHeight: 16
+//     });
+//     this.load.spritesheet("ship2", "../../assets/spritesheets/ship2.png",{
+//       frameWidth: 32,
+//       frameHeight: 16
+//     });
+//     this.load.spritesheet("ship3", "../../assets/spritesheets/ship3.png",{
+//       frameWidth: 32,
+//       frameHeight: 32
+//     });
+//     this.load.spritesheet("explosion", "../../assets/spritesheets/explosion.png",{
+//       frameWidth: 16,
+//       frameHeight: 16
+//     });
+//     // 2.1 load the spritesheet
+//     this.load.spritesheet("power-up", "../../assets/spritesheets/power-up.png",{
+//       frameWidth: 16,
+//       frameHeight: 16
+//     });
+
+//   }
+
+//   create() {
+//     this.add.text(20, 20, "Loading game...");
+//     this.add.triangle(50,50);
+//     this.scene.start('MainScene');
+//   }
+// }
   constructor() {
     super({ key: 'PreloadScene' });
   }
@@ -7,6 +44,6 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   create() {
-    this.scene.start('MainScene');
+    this.add.text(20, 20, "Loading game...");
   }
 }
